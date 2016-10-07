556c4ca698870c56cbaa52792ddc5c60b9e3dc98
Charles Renwick <charles.srenwick@gmail.com>
2016-10-07 00:43:11 -0400

    toggling seats

--- web/elm/SeatSaver.elm
+++ web/elm/SeatSaver.elm
@@ -1,7 +1,72 @@
 
 module SeatSaver exposing (..)
 
-import Html
+import Html exposing (..)
+import Html.Attributes exposing (class)
+import Html.App as App
+import Html.Events exposing (onClick)
 
+main : Program Never
 main =
-  Html.text "Hello from Elm"
+  App.beginnerProgram
+    { model = init
+    , update = update
+    , view = view
+    }
+
+-- MODEL
+
+type alias Seat =
+  { seatNo : Int, occupied: Bool }
+
+type alias Model =
+  List Seat
+
+init : Model
+init =
+  [ { seatNo = 1, occupied = False }
+  , { seatNo = 2, occupied = False }
+  , { seatNo = 3, occupied = False }
+  , { seatNo = 4, occupied = False }
+  , { seatNo = 5, occupied = False }
+  , { seatNo = 6, occupied = False }
+  , { seatNo = 7, occupied = False }
+  , { seatNo = 8, occupied = False }
+  , { seatNo = 9, occupied = False }
+  , { seatNo = 10, occupied = False }
+  , { seatNo = 11, occupied = False }
+  , { seatNo = 12, occupied = False }
+  ]
+
+-- UPDATE
+
+type Msg = Toggle Seat
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Toggle seatToToggle ->
+      let updateSeat seatFromModel =
+        if seatFromModel.seatNo == seatToToggle.seatNo then
+          { seatFromModel | occupied = not seatFromModel.occupied }
+        else seatFromModel
+      in
+        List.map updateSeat model
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+  ul [ class "seats" ] (List.map seatItem model)
+
+seatItem : Seat -> Html Msg
+seatItem seat =
+  let
+    occupiedClass =
+      if seat.occupied then "occupied" else "available"
+  in
+    li
+      [ class ("seat " ++ occupiedClass)
+      , onClick (Toggle seat)
+      ] 
+      [ text (toString seat.seatNo) ]
